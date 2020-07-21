7adf05a447969f2afb26392b5d7cda0268841d70
Bruna Gadelha <bruna.gadelha@ubus.com.br>
2020-07-21 00:15:11 -0300

    Discovering resources of Xamarin.Forms

---
+++ HelloXamarin/MainPage.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using HelloXamarin.Models;
+using System.Collections.ObjectModel;
+
+namespace HelloXamarin
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public ObservableCollection<ProductItem> Items { get; set; } = new ObservableCollection<ProductItem>();
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            ItemsList.ItemsSource = Items;
+            ItemsList.ItemSelected += OnItemSelected; 
+        }
+
+        async void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            var item = (ProductItem)e.SelectedItem;
+
+            bool answer = await DisplayAlert("Alerta", $"Deseja excluir o item {item.Name}?", "Sim", "Não");
+
+            if (answer)
+            {
+                Items.Remove(item);
+                RedefinirTotal(); 
+            }
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(Quantity.Text) ||
+                string.IsNullOrEmpty(Value.Text) ||
+                string.IsNullOrEmpty(ProductName.Text))
+            {
+                DisplayAlert("Alerta", "Preencha todos os campos", "OK");
+                return;
+            }
+
+            var item = new ProductItem()
+            {
+                Name = ProductName.Text,
+                Quantity = Convert.ToInt32(Quantity.Text),
+                Value = Convert.ToDecimal(Value.Text),
+            };
+
+            Items.Add(item);
+
+            ResetForm();
+
+
+
+            RedefinirTotal(); 
+
+
+
+        }
+
+        private void ResetForm()
+        {
+            ProductName.Text = "";
+            Quantity.Text = "";
+            Value.Text = "";
+        }
+
+        private void RedefinirTotal()
+        {
+            Total.Text = string.Format("{0:C}", Items.Sum(x => x.Total));
+        }
+    }
+}
---
+++ HelloXamarin/Models/ProductItem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloXamarin.Models
+{
+    public class ProductItem
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal Value { get; set; } = 0;
+        public string FormattedValue
+        {
+            get
+            {
+                return string.Format("{0:C}", Value);
+            }
+        }
+        public decimal Total { get { return Value * Quantity; } }
+        public string FormattedTotal
+        {
+            get
+            {
+                return string.Format("{0:C}", Total);
+            }
+        }
+
+        //public override string ToString()
+        //{
+        //    return $"{this.Quantity} {this.Name.ToString()} - {this.Value.ToString()}";
+        //}
+    }
+}
